fac1021c85ba8f0f7bbf13f54f36cbc262246e03
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-01-05 17:33:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    User.new(session[:user_id])
+    User.find_by(id:session[:user_id])
   end
 
   helper_method :current_user
