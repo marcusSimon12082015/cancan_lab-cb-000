35a449e4e4ebfaae7fe5cd27f11d945b49c282c1
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-01-05 17:36:45 +0000

    Automatically backed up by Learn

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,10 +1,10 @@
 class SessionController < ApplicationController
   def create
     user = User.find_by(name:params[:name])
-    session[:id] = user.id
+    session[:user_id] = user.id
   end
 
   def destroy
-    session.delete(:id)
+    session.delete(:user_id)
   end
 end
