fb0b817e5aa9505be6c4dfbfe5c7f045a30caf52
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-01-05 17:33:39 +0000

    Automatically backed up by Learn

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -5,7 +5,7 @@ class NotesController < ApplicationController
     user = User.find_by(session[:user_id])
     @note.user = user unless user.nil?
     @note.readers << user unless user.nil?
-    byebug
+    #byebug
     redirect_to '/'
   end
 
