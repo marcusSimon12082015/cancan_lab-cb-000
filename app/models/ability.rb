7b576dad6187d423fbee36545225ec80f155d683
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-01-05 16:24:19 +0000

    Automatically backed up by Learn

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,8 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :read,:update, Note, user_id: user.id
+    can :read, Note, user_id: user.id
+    can :update, Note, user_id: user.id
     can :read, Note do |note|
       !Viewer.find_by(user_id: user.id, note_id: note.id).nil?
     end
