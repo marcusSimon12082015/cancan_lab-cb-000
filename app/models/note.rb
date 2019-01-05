4e3e54893f547e7ca980d38e7fc2152c26f8e744
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-01-05 14:03:14 +0000

    Automatically backed up by Learn

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,10 @@
 class Note < ActiveRecord::Base
   has_many :viewers
   has_many :readers, through: :viewers, source: :user
+
+  def visible_to=(readers)
+    readers.split(',').each do |reader|
+
+    end 
+  end 
 end
