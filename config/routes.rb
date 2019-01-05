ec47a46083cb9ceb8303ed2d99f175d3d3693e81
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-01-05 18:44:37 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get 'user/create'
 
-  post "/notes/:id" => "notes#update"
+  post "notes/:id" => "notes#update"
   resources :notes, :except => [:update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
