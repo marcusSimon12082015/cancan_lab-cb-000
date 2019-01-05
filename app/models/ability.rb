971515b08e7f122a9cb58203dd23a8a1db6d7cd4
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-01-05 16:25:35 +0000

    Automatically backed up by Learn

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,9 +4,9 @@ class Ability
   def initialize(user)
     can :read, Note, user_id: user.id
     can :update, Note, user_id: user.id
-    can :read, Note do |note|
-      !Viewer.find_by(user_id: user.id, note_id: note.id).nil?
-    end
+#    can :read, Note do |note|
+#      !Viewer.find_by(user_id: user.id, note_id: note.id).nil?
+#    end
 
     cannot :read, Note do |note|
       Viewer.where(user_id: user.id, note_id: note.id) == []
