da00f0d56b558efaa1fb5faa871b5dbfa39620a8
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-01-05 18:00:44 +0000

    Automatically backed up by Learn

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe NotesController, type: :controller do
     it "can update your own notes" do
       alice, beth = users(:alice), users(:beth)
       session[:user_id] = beth.id
-      
+
       content = 'oh so secret'
       post :create, note: {content: content, visible_to: ''}
       note_id = Note.last.id
